a0d4995b626caa6535350b940d0c78e084801784
meku <piangree147@gmail.com>
2023-02-11 22:25:29 +0800

    Completed Testimonials, Clients, Feedback Card, and half way through CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import styles from "../style";
+import Button from "./Button";
 
-const CTA = () => {
-  return (
-    <div>CTA</div>
-  )
-}
+const CTA = () => (
+  <section>
+    <div>
+      <h2>Let’s try our service now!</h2>
+      <p>
+        Everything you need to accept card payments and grow your business
+        anywhere on the planet.
+      </p>
+    </div>
+    <Button />
+  </section>
+);
 
-export default CTA
+export default CTA;
